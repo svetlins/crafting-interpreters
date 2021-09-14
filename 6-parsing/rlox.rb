86e23fa0b75755d060dfc00b36ea4d474db0e31b
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-15 00:11:38 +0300

    A commit

---
+++ 6-parsing/rlox.rb
@@ -0,0 +1,54 @@
+$LOAD_PATH.unshift File.join(__dir__, 'lib')
+
+require 'scanner'
+require 'expression'
+
+class Rlox
+  def initialize(argv)
+    @argv = argv
+    @@had_error = false
+  end
+
+  def main
+    if @argv.size > 1
+      puts "Usage: rlox [source_file]"
+      exit(64)
+    elsif @argv.size == 1
+      run_file(@argv.first)
+    else
+      run_prompt
+    end
+  end
+
+  def run_file(file_name)
+    run(File.read(file_name))
+    exit(65) if @@had_error
+  end
+
+  def run_prompt
+    loop do
+      print '> '
+      line = $stdin.gets
+      break if line.nil?
+      run(line)
+      @@had_error = false
+    end
+  end
+
+  def run(source)
+    scanner = Scanner.new(source)
+    tokens = scanner.scan
+
+    tokens.each do |token|
+      p token
+    end
+  end
+
+  def self.report_error(line, message)
+    $stderr.puts "line: #{line} - error: #{message}"
+    @@had_error = true
+  end
+end
+
+Expression.test_mest
+Rlox.new(ARGV).main

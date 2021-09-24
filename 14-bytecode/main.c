5609864daff1f60b95ac61da11423e9828dd0c1a
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-24 10:03:47 +0300

    Fuck

---
+++ 14-bytecode/main.c
@@ -0,0 +1,13 @@
+#include "common.h"
+#include "chunk.h"
+
+int main(int argc, char *argv[])
+{
+  Chunk chunk;
+
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  freeChunk(&chunk);
+
+  return 0;
+}

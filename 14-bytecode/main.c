c40bd117d36825f8dc46fb557c09f0cf2c2939b5
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-25 19:59:48 +0300

    A commit

--- 14-bytecode/main.c
+++ 14-bytecode/main.c
@@ -1,12 +1,19 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, char *argv[])
 {
   Chunk chunk;
 
   initChunk(&chunk);
-  writeChunk(&chunk, OP_RETURN);
+
+  int constant = addConstant(&chunk, 42);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+  disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
 
   return 0;

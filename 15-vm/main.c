c3e3289ba1a48a83a46f98c6c6009e44f25f54d0
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-26 16:13:17 +0300

    A commit

--- 15-vm/main.c
+++ 15-vm/main.c
@@ -10,9 +10,22 @@ int main(int argc, char *argv[])
 
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 42);
+  // int constant = addConstant(&chunk, 42);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
+  // writeChunk(&chunk, OP_NEGATE, 123);
+
+  int a = addConstant(&chunk, 1.2);
+  int b = addConstant(&chunk, 3.4);
+  int c = addConstant(&chunk, 5.6);
   writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, a, 123);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, b, 123);
+  writeChunk(&chunk, OP_ADD, 123);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, c, 123);
+  writeChunk(&chunk, OP_DIVIDE, 123);
   writeChunk(&chunk, OP_NEGATE, 123);
 
   writeChunk(&chunk, OP_RETURN, 124);
--- 15-vm/vm.c
+++ 15-vm/vm.c
@@ -34,6 +34,14 @@ static InterpretResult run()
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+#define BINARY_OP(op) \
+  do                  \
+  {                   \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
+
   for (;;)
   {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -56,6 +64,18 @@ static InterpretResult run()
       push(-pop());
       break;
     }
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
     case OP_RETURN:
     {
       printValue(pop());
@@ -73,6 +93,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)

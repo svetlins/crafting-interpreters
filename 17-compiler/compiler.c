9cf4ee67f0d7211535eb5e95c93d3f62d86c5b37
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-28 00:20:11 +0300

    A commit

--- 17-compiler/compiler.c
+++ 17-compiler/compiler.c
@@ -1,27 +1,159 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "chunk.h"
 
-void compile(const char *source)
+typedef struct
 {
-  initScanner(source);
-  int line = -1;
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk()
+{
+  return compilingChunk;
+}
+
+static void errorAt(Token *token, const char *message)
+{
+  if (parser.panicMode)
+    return;
+
+  parser.panicMode = true;
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF)
+  {
+    fprintf(stderr, " at end");
+  }
+  else if (token->type == TOKEN_ERROR)
+  {
+  }
+  else
+  {
+    fprintf(stderr, "at' %.*s", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void error(const char *message)
+{
+  errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message)
+{
+  errorAt(&parser.current, message);
+}
+
+static void advance()
+{
+  parser.previous = parser.current;
+
   for (;;)
   {
-    Token token = scanToken();
-    if (token.line != line)
-    {
-      printf("%4d ", token.line);
-      line = token.line;
-    }
-    else
-    {
-      printf("   | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
-    if (token.type == TOKEN_EOF)
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR)
       break;
+
+    errorAtCurrent(parser.current.start);
   }
+}
+
+static void consume(TokenType type, const char *message)
+{
+  if (parser.current.type == type)
+  {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte)
+{
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn()
+{
+  emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value)
+{
+  int constant = addConstant(currentChunk(), value);
+
+  if (constant > UINT8_MAX)
+  {
+    error("Too many constants in one chunk");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
+static void emitConstant(Value value)
+{
+  emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler()
+{
+  emitReturn();
+}
+
+static void grouping()
+{
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expected ) after expression");
+}
+
+static void number()
+{
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+static void unary()
+{
+  TokenType operatorType = parser.previous.type;
+  expression();
+  switch (operatorType)
+  {
+  case TOKEN_MINUS:
+    emitByte(OP_NEGATE);
+    break;
+  }
+}
+
+void compile(const char *source, Chunk *chunk)
+{
+  initScanner(source);
+  compilingChunk = chunk;
+  parser.hadError = false;
+  parser.panicMode = false;
+
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expected end of expression");
+  endCompiler();
+  return !parser.hadError;
 }

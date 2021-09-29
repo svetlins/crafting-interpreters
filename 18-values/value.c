87de480ce7c97c4564213478655b1051c32fe802
Svetlin Simonyan <svetlin.s@gmail.com>
2021-09-29 17:42:03 +0300

    A commit

---
+++ 18-values/value.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include "memory.h"
+#include "value.h"
+
+void initValueArray(ValueArray *array)
+{
+  array->capacity = 0;
+  array->count = 0;
+  array->values = NULL;
+}
+void writeValueArray(ValueArray *array, Value value)
+{
+  if (array->capacity < array->count + 1)
+  {
+    int oldCapacity = array->capacity;
+    array->capacity = GROW_CAPACITY(oldCapacity);
+    array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+  }
+
+  array->values[array->count] = value;
+  array->count++;
+}
+
+void freeValueArray(ValueArray *array)
+{
+  FREE_ARRAY(Value, array->values, array->capacity);
+  initValueArray(array);
+}
+
+void printValue(Value value)
+{
+  if (value.type == VAL_NUMBER)
+    printf("%g", AS_NUMBER(value));
+  else if (value.type == VAL_BOOL)
+    printf(AS_BOOL(value) ? "true" : "false");
+  else if (value.type == VAL_NIL)
+    printf("nil");
+}

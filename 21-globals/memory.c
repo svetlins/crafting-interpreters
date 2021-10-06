f7b7b0d2f2ec4d4f97cdc537ab1f97afe7e78e7e
Svetlin Simonyan <svetlin.s@gmail.com>
2021-10-06 09:17:44 +0300

    A commit

---
+++ 21-globals/memory.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+#include "memory.h"
+#include "vm.h"
+
+void *reallocate(void *pointer, size_t oldSize, size_t newSize)
+{
+  if (newSize == 0)
+  {
+    free(pointer);
+    return NULL;
+  }
+
+  void *result = realloc(pointer, newSize);
+
+  if (result == NULL)
+    exit(1);
+
+  return result;
+}
+
+static void freeObject(Obj *obj)
+{
+  switch (obj->type)
+  {
+  case OBJ_STRING:
+  {
+    ObjString *string = (ObjString *)obj;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, obj);
+    break;
+  }
+  }
+}
+
+void freeObjects()
+{
+  Obj *currentObject = vm.objects;
+
+  while (currentObject != NULL)
+  {
+    Obj *nextObject = currentObject->next;
+    freeObject(currentObject);
+    currentObject = nextObject;
+  }
+}

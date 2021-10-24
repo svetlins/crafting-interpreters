89b50e49c95d92a72f3946edf4063f17e0144abb
Svetlin Simonyan <svetlin.s@gmail.com>
2021-10-24 14:45:22 +0300

    Futzing with fucking Rust

--- rust-lox/src/scanner.rs
+++ rust-lox/src/scanner.rs
@@ -71,7 +71,7 @@ impl<'a> Iterator for Scan<'a> {
       return None;
     }
 
-    Some(self.scan_token())
+    self.scan_token()
   }
 }
 
@@ -96,42 +96,35 @@ impl<'a> Scan<'a> {
       panic!("Can't advance beyond end");
     }
 
-    let current_char = self.current_char.unwrap();
+    let consumed_char = self.current_char.unwrap();
 
     self.current_char = self.next_char;
     self.next_char = self.chars.next();
 
-    self.current_token.push(current_char);
+    self.current_token.push(consumed_char);
 
-    current_char
+    consumed_char
   }
 
-  fn matches(&mut self, c: char) -> bool {
-    if self.peek(c) {
-      self.advance();
-      true
-    } else {
-      false
+  fn matches(&mut self, other: char) -> bool {
+    match self.peek() {
+      Some(c) if c == other => {
+        self.advance();
+        true
+      }
+      _ => false,
     }
   }
 
-  fn peek(&self, c: char) -> bool {
-    if let Some(current_char) = self.current_char {
-      current_char == c
-    } else {
-      false
-    }
+  fn peek(&self) -> Option<char> {
+    self.current_char
   }
 
-  fn peek_next(&self, c: char) -> bool {
-    if let Some(next_char) = self.next_char {
-      next_char == c
-    } else {
-      false
-    }
+  fn peek_next(&self) -> Option<char> {
+    self.next_char
   }
 
-  pub fn scan_token(&mut self) -> Token {
+  pub fn scan_token(&mut self) -> Option<Token> {
     if self.at_end() {
       return self.make_token(TokenType::Eof);
     }
@@ -194,17 +187,34 @@ impl<'a> Scan<'a> {
     }
   }
 
-  fn scan_identifier(&mut self) -> Token {
+  fn scan_identifier(&mut self) -> Option<Token> {
     self.advance();
     self.make_token(TokenType::Error)
   }
 
-  fn scan_number(&mut self) -> Token {
-    panic!("ko 6i praim");
+  fn scan_number(&mut self) -> Option<Token> {
+    let mut saw_decimal_point = false;
+
+    loop {
+      match self.peek() {
+        Some(c) if c.is_ascii_digit() => {
+          self.advance();
+          ()
+        }
+        Some(c) if c == '.' && !saw_decimal_point => {
+          saw_decimal_point = true;
+          self.advance();
+          ()
+        }
+        _ => break (),
+      }
+    }
+
+    self.make_token(TokenType::Number)
   }
 
-  fn scan_string(&mut self) -> Token {
-    while !self.peek('"') {
+  fn scan_string(&mut self) -> Option<Token> {
+    while self.peek() != Some('"') {
       self.advance();
     }
 
@@ -218,7 +228,7 @@ impl<'a> Scan<'a> {
       return;
     }
 
-    while self.peek(' ') {
+    while self.peek() == Some(' ') {
       self.advance();
     }
   }
@@ -227,12 +237,12 @@ impl<'a> Scan<'a> {
     self.current_char == None
   }
 
-  fn make_token(&self, token_type: TokenType) -> Token {
-    Token {
+  fn make_token(&self, token_type: TokenType) -> Option<Token> {
+    Some(Token {
       token_type: token_type,
       text: self.current_token.clone(),
       line: self.line,
-    }
+    })
   }
 }
 
@@ -248,58 +258,61 @@ mod tests {
   #[test]
   fn test_single_char_op() {
     let mut scan = Scan::new("+");
-    assert_eq!(scan.scan_token().token_type, TokenType::Plus);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Plus);
   }
 
   #[test]
   fn test_multiple_single_char_op() {
     let mut scan = Scan::new("+-*");
-    assert_eq!(scan.scan_token().token_type, TokenType::Plus);
-    assert_eq!(scan.scan_token().token_type, TokenType::Minus);
-    assert_eq!(scan.scan_token().token_type, TokenType::Star);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Plus);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Minus);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Star);
   }
 
   #[test]
   fn test_whitespace() {
     let mut scan = Scan::new("       +         ");
-    assert_eq!(scan.scan_token().token_type, TokenType::Plus);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Plus);
   }
 
   #[test]
   fn test_double_char_op() {
     let mut scan = Scan::new("!=");
-    assert_eq!(scan.scan_token().token_type, TokenType::BangEqual);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::BangEqual);
   }
 
   #[test]
   fn test_double_char_confusion_op() {
     let mut scan = Scan::new("!");
-    assert_eq!(scan.scan_token().token_type, TokenType::Bang);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Bang);
   }
 
   #[test]
   fn test_greater() {
     let mut scan = Scan::new(">");
-    assert_eq!(scan.scan_token().token_type, TokenType::Greater);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Greater);
   }
 
   #[test]
   fn test_greater_equal() {
     let mut scan = Scan::new(">=");
-    assert_eq!(scan.scan_token().token_type, TokenType::GreaterEqual);
+    assert_eq!(
+      scan.scan_token().unwrap().token_type,
+      TokenType::GreaterEqual
+    );
   }
 
   #[test]
   fn test_string() {
     let mut scan = Scan::new("\"some string\"");
-    assert_eq!(scan.scan_token().token_type, TokenType::String);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::String);
   }
 
   #[test]
   fn test_string_and_after() {
     let mut scan = Scan::new("\"some string\"+");
     scan.scan_token();
-    assert_eq!(scan.scan_token().token_type, TokenType::Plus);
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Plus);
   }
 
   #[test]
@@ -327,6 +340,100 @@ mod tests {
   #[test]
   fn test_integer() {
     let mut scan = Scan::new("42");
-    assert_eq!(scan.scan_token().token_type, TokenType::Number);
+    let token = scan.scan_token().unwrap();
+    assert_eq!(token.token_type, TokenType::Number);
+    assert_eq!(token.text, String::from("42"));
+  }
+
+  #[test]
+  fn test_decimal() {
+    let mut scan = Scan::new("42.69");
+    let token = scan.scan_token().unwrap();
+    assert_eq!(token.token_type, TokenType::Number);
+    assert_eq!(token.text, String::from("42.69"));
+  }
+
+  #[test]
+  fn test_malformed_decimal() {
+    let mut scan = Scan::new("42.69.666");
+    let token = scan.scan_token().unwrap();
+    assert_eq!(token.token_type, TokenType::Number);
+    assert_eq!(token.text, String::from("42.69"));
+  }
+
+  #[test]
+  fn test_empty() {
+    let mut scan = Scan::new("");
+    assert_eq!(scan.scan_token().unwrap().token_type, TokenType::Eof);
+  }
+}
+
+// ---------------------------------
+
+struct DoublePeekable<I: Iterator> {
+  iter: I,
+  first_peeked: Option<Option<I::Item>>,
+  second_peeked: Option<Option<I::Item>>,
+}
+
+impl<I: Iterator> DoublePeekable<I> {
+  pub fn new(iter: I) -> DoublePeekable<I> {
+    DoublePeekable {
+      iter,
+      first_peeked: None,
+      second_peeked: None,
+    }
+  }
+}
+
+impl<I: Iterator> Iterator for DoublePeekable<I> {
+  type Item = I::Item;
+
+  fn next(&mut self) -> Option<I::Item> {
+    match self.first_peeked.take() {
+      Some(v) => {
+        self.first_peeked = self.second_peeked.take();
+        self.second_peeked = None;
+        v
+      }
+      None => self.iter.next(),
+    }
+  }
+}
+
+impl<I: Iterator> DoublePeekable<I> {
+  pub fn peek(&mut self) -> Option<&I::Item> {
+    let iter = &mut self.iter;
+    self
+      .first_peeked
+      .get_or_insert_with(|| iter.next())
+      .as_ref()
+  }
+
+  pub fn peek_next(&mut self) -> Option<&I::Item> {
+    let iter = &mut self.iter;
+
+    self.first_peeked.get_or_insert_with(|| iter.next());
+
+    self
+      .second_peeked
+      .get_or_insert_with(|| iter.next())
+      .as_ref()
+  }
+}
+
+#[cfg(test)]
+mod tests2 {
+  use super::*;
+
+  #[test]
+  fn test_double_peekable() {
+    let vec: Vec<u32> = Vec::from([1, 2, 3, 4, 5]);
+    let mut double_peekable = DoublePeekable::new(vec.iter());
+
+    println!("HIIiiiiiiiiii {:?}", double_peekable.next());
+    println!("HIIiiiiiiiiii {:?}", double_peekable.peek());
+    println!("HIIiiiiiiiiii {:?}", double_peekable.peek_next());
+    println!("HIIiiiiiiiiii {:?}", double_peekable.next());
   }
 }

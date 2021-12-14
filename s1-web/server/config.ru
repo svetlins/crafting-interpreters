e02fa42528a7e55a2e913d9a6ffec157cfc4cb8d
Svetlin Simonyan <svetlin.s@gmail.com>
2021-12-14 11:24:05 +0200

    Bundler stuff

--- s1-web/server/config.ru
+++
@@ -1,9 +0,0 @@
-require 'bundler/setup'
-Bundler.require(:default)
-
-require File.join(File.dirname(__FILE__), "web")
-
-
-map "/" do
-    run LoxWeb
-end
--- s1-web/server/web.rb
+++ s1-web/server/web.rb
@@ -1,27 +1,29 @@
+require 'sinatra'
+require 'sinatra/cors'
+require 'rack/contrib'
+
 $LOAD_PATH.unshift File.join(__dir__, 'lib')
 
 require 'scanner'
 require 'parser'
 require 'printers/tree_printer'
 
-class LoxWeb < Sinatra::Base
-  set :allow_origin, "*"
-  set :allow_methods, "GET,HEAD,POST"
-  set :allow_headers, "content-type,if-modified-since"
-  set :expose_headers, "location,link"
+set :allow_origin, "*"
+set :allow_methods, "GET,HEAD,POST"
+set :allow_headers, "content-type,if-modified-since"
+set :expose_headers, "location,link"
 
-  use Rack::JSONBodyParser
+use Rack::JSONBodyParser
 
-  before do
-    content_type :json
-  end
+before do
+  content_type :json
+end
 
-  post '/tokens' do
-    source = request.params["source"]
+post '/tokens' do
+  source = request.params["source"]
 
-    tokens = Scanner.new(source).scan
-    tree = TreePrinter.print(Parser.new(tokens).parse)
+  tokens = Scanner.new(source).scan
+  tree = TreePrinter.print(Parser.new(tokens).parse)
 
-    {tokens: tokens.map(&:as_json), tree: tree}.to_json
-  end
+  {tokens: tokens.map(&:as_json), tree: tree}.to_json
 end

69f595192af7c5b722d1a9eb8dd679cbf3004bbe
Svetlin Simonyan <svetlin.s@gmail.com>
2021-12-14 11:13:32 +0200

    Bundler stuff

--- s1-web/server/web.rb
+++ s1-web/server/web.rb
@@ -1,30 +1,27 @@
-require 'rubygems'
-require 'bundler'
-
-Bundler.require
-
 $LOAD_PATH.unshift File.join(__dir__, 'lib')
 
 require 'scanner'
 require 'parser'
 require 'printers/tree_printer'
 
-set :allow_origin, "*"
-set :allow_methods, "GET,HEAD,POST"
-set :allow_headers, "content-type,if-modified-since"
-set :expose_headers, "location,link"
+class LoxWeb < Sinatra::Base
+  set :allow_origin, "*"
+  set :allow_methods, "GET,HEAD,POST"
+  set :allow_headers, "content-type,if-modified-since"
+  set :expose_headers, "location,link"
 
-use Rack::JSONBodyParser
+  use Rack::JSONBodyParser
 
-before do
-  content_type :json
-end
+  before do
+    content_type :json
+  end
 
-post '/tokens' do
-  source = request.params["source"]
+  post '/tokens' do
+    source = request.params["source"]
 
-  tokens = Scanner.new(source).scan
-  tree = TreePrinter.print(Parser.new(tokens).parse)
+    tokens = Scanner.new(source).scan
+    tree = TreePrinter.print(Parser.new(tokens).parse)
 
-  {tokens: tokens.map(&:as_json), tree: tree}.to_json
+    {tokens: tokens.map(&:as_json), tree: tree}.to_json
+  end
 end
